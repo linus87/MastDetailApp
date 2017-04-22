48d06fc49be7c065be51bcccc17ce319fdd6621d
lyan2 <lyan2@ebay.com>
2017-04-22 15:04:11 +0800

    first commit

--- MastDetailApp/main.m
+++ MastDetailApp/main.m
@@ -7,10 +7,41 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 #import "AppDelegate.h"
 
+typedef void (^CompletionBlock)();
+
+@interface SampleClass:NSObject
+
+- (void) performActionWithCompletion:(CompletionBlock) completionBlock;
+
+@end
+
+@implementation SampleClass
+
+- (void) performActionWithCompletion:(CompletionBlock)completionBlock {
+	NSLog(@"Action Performed");
+	
+	completionBlock();
+}
+
+@end
+
 int main(int argc, char * argv[]) {
+	NSLog(@"hello world, this is write by Linus Yan.");
+	
+	SampleClass *sampleClass = [[SampleClass alloc] init];
+	
+	[sampleClass performActionWithCompletion:^{
+		NSLog(@"Completion is called to intimate action is performed.");
+	}];
+	
+	
 	@autoreleasepool {
+		
 	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
 	}
+	
+	return 0;
 }
